7ad0c234d1281c876a44c98d83e9f796f94de44d
John Hutchison <john.hutchison@nau.edu>
2015-08-04 16:34:14 -0700

    Removed ignores

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,92 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.email-composer/www/email_composer.js",
+        "id": "de.appplant.cordova.plugin.email-composer.EmailComposer",
+        "clobbers": [
+            "cordova.plugins.email",
+            "plugin.email"
+        ]
+    },
+    {
+        "file": "plugins/nl.codeyellow.signature/www/signature-view-native.js",
+        "id": "nl.codeyellow.signature.Signature"
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.OrientationLock/www/orientationLock.js",
+        "id": "com.phonegap.plugins.OrientationLock.OrientationLock",
+        "clobbers": [
+            "OrientationLock"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-device": "1.0.0",
+    "cordova-plugin-statusbar": "1.0.0",
+    "cordova-plugin-network-information": "1.0.0",
+    "cordova-plugin-dialogs": "1.1.0",
+    "com.phonegap.plugins.barcodescanner": "2.2.0",
+    "de.appplant.cordova.plugin.email-composer": "0.8.2",
+    "nl.codeyellow.signature": "0.1",
+    "com.phonegap.plugins.OrientationLock": "0.1"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/android/assets/www/scripts/app.js
@@ -0,0 +1,650 @@
+/* global angular */
+'use strict'
+angular.module('main', [
+  'ionic',
+  'ngCordova',
+  'ui.router'
+])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/start')
+
+    // some basic routing
+    $stateProvider
+      .state('start', {
+        url: '/start',
+        templateUrl: 'main/templates/start.html',
+        controller: 'Start as start'
+      })
+      .state('scan', {
+        url: '/scan',
+        templateUrl: 'main/templates/scan.html',
+        controller: 'Scan as scan'
+      })
+      .state('sign', {
+        url: '/sign',
+        templateUrl: 'main/templates/sign.html',
+        controller: 'Sign as sign'
+      })
+      .state('view', {
+        url: '/view',
+        templateUrl: 'main/templates/view.html',
+        controller: 'View as view'
+      })
+  })
+
+/* global angular, cordova */
+'use strict'
+angular.module('main')
+  .factory('Signature', function ($log, $q) {
+    return {
+      getSignature: function (title, htmlPage) {
+        title = title || 'Please sign'
+        htmlPage = htmlPage || ''
+
+        var Signature = cordova.require('nl.codeyellow.signature.Signature')
+
+        return $q(function (resolve, reject) {
+          Signature.getSignature(resolve, reject, title, 'Done', '', htmlPage)
+        })
+      }
+    }
+
+  })
+
+/* global angular, cordova */
+'use strict'
+angular.module('main')
+  .factory('Email', function ($log, $q) {
+    return {
+      isAvailable: function () {
+        var q = $q.defer()
+
+        cordova.plugins.email.isAvailable(q.resolve)
+
+        return q.promise
+      },
+
+      open: function (params) {
+        // to:          Array, // email addresses for TO field
+        // cc:          Array, // email addresses for CC field
+        // bcc:         Array, // email addresses for BCC field
+        // attachments: Array, // file paths or base64 data streams
+        // subject:    String, // subject of the email
+        // body:       String, // email body (for HTML, set isHtml to true)
+        // isHtml:    Boolean, // indicats if the body is HTML or plain text
+
+        return $q(function (resolve, reject) {
+          cordova.plugins.email.open(params, resolve)
+        })
+      }
+    }
+
+  })
+
+/* global angular, vCard */
+'use strict'
+angular.module('main')
+  .factory('Data', function ($window, $q, $log, Config, Barcode, Signature) {
+    var Data = function () {
+      this.debug = $log.debug || console.debug || console.log
+      this.reset()
+
+    // this.debug($window.localStorage.records)
+    }
+
+    // Database r/w
+
+    Data.prototype.isValid = function () {
+      return this.currentScan && this.currentSignature
+    }
+
+    Data.prototype.reset = function () {
+      this.currentScan = null
+      this.currentSignature = null
+    }
+
+    Data.prototype.set = function (key, value) {
+      $window.localStorage[key] = JSON.stringify(value)
+    }
+
+    Data.prototype.get = function (key, deflt) {
+      return JSON.parse($window.localStorage[key] || 'null') || deflt || {}
+    }
+
+    Data.prototype.save = function () {
+      var q = $q.defer()
+      var record
+
+      if (this.isValid()) {
+        record = angular.copy(this.currentScan)
+
+        record.id = Date.now()
+        record.created = new Date().toISOString()
+        record.signature = this.currentSignature
+
+        this.insert(record)
+        q.resolve(record)
+      } else {
+        q.reject(Error('Current scan and signature required to save', 1000))
+      }
+
+      return q.promise
+    }
+
+    Data.prototype.saveAll = function (records) {
+      if (angular.isArray(records)) {
+        this.set('records', records)
+      }
+    }
+
+    Data.prototype.getAll = function () {
+      return this.get('records', [])
+    }
+
+    Data.prototype.find = function (params) {
+      if (typeof params === 'string' || typeof params === 'number') {
+        params = { id: params }
+      }
+
+      return this.getAll().filter(function (r) {
+        return Object.keys(params || {}).every(function (k) {
+          return String(r[k]) === String(params[k])
+        })
+      })
+    }
+
+    Data.prototype.insert = function (record) {
+      var records = this.getAll()
+      records.push(record)
+      this.saveAll(records)
+    }
+
+    Data.prototype.delete = function (params) {
+      if (typeof params === 'string' || typeof params === 'number') {
+        params = { id: params }
+      }
+
+      this.set('records', this.getAll().filter(function (r) {
+        return !Object.keys(params).every(function (k) {
+            return String(r[k]) === String(params[k])
+          })
+      }))
+    }
+
+    // Scanning
+
+    Data.prototype.scan = function () {
+      this.currentScan = null
+
+      return Barcode.scan()
+        .then(angular.bind(this, function (result) {
+          this.debug('[Data] Raw scan result: ' + JSON.stringify(result))
+
+          // Todo: format / validate scanned data
+          if (!result || result.cancelled) {
+            return $q.reject(Error('Cancelled by user', 1001))
+          }
+
+          // Removed this validation. As long as the scan results in a vCard,
+          // we don't really card if it's a QR
+          // if (result.format !== 'QR_CODE') {
+          //   return $q.reject(Error('Not a QR Code', 1002))
+          // }
+
+          if (!/^BEGIN:VCARD/i.test(result.text)) {
+            return $q.reject(Error('Scanned data is not a vCard', 1003))
+          }
+
+          // Parse the data
+          var data = vCard.parse(result.text) // result.text.split('\n')
+          var labels = { 'n': 'name', 'fn': 'fullname', 'adr': 'address', 'tel': 'telephone' }
+          var required = { 'n': true }
+          var formatted = { vCard: result.text }
+          var k
+
+          // this.debug('[Data] Formatted data: ' + JSON.stringify(data))
+
+          // Validate required
+          for (k in required) {
+            if (!(data[k] && data[k][0] && data[k][0].value)) {
+              return $q.reject(Error(labels[k] + ' is required', 1010))
+            }
+          }
+
+          // Add included fields
+          var processValue = function (value) {
+            if (value.length > 1) {
+              // array of values
+              if (value.every(function (o) { return o.meta && o.meta.type })) {
+                // Each sub object has a type listed
+                var temp = {}
+                value.forEach(function (o) {
+                  temp[o.meta.type[0].toLowerCase().replace(/[^\w]/g, '_')] = o.value
+                })
+                return temp
+
+              } else {
+                // no types
+                return value.map(function (o) {
+                  return o.value
+                })
+              }
+            } else {
+              // simple key and value
+              return angular.isArray(value[0].value)
+                ? value[0].value.filter(function (o) { return !!o })
+                : value[0].value
+            }
+          }
+
+          for (k in data) {
+            if (data.hasOwnProperty(k)) {
+              this.debug('[Data] ' + k + ' -> ' + JSON.stringify(data[k]))
+              formatted[labels[k] || k] = processValue(data[k])
+            }
+          }
+
+          // Find number in house if it's there
+          var match = formatted.vCard.match(/([A-Z]+):num(ber)? ?in ?house[^\d]+(\d+)/i)
+          if (match) {
+            this.debug('[Data] Number in house found in field ' + match[1] + ': ' + match[3])
+
+            formatted.number_in_house = match[3]
+            formatted.number_in_house_field = match[1]
+          }
+
+          // Done Parsing
+
+          this.currentScan = formatted
+          return this.currentScan
+        }))
+    }
+
+    // Signing
+
+    Data.prototype.sign = function (title, htmlPage) {
+      this.currentSignature = null
+
+      return Signature.getSignature(title, htmlPage)
+        .then(angular.bind(this, function (result) {
+          this.debug('[Data] Sign complete')
+
+          // Format / validate signature data
+
+          if (!result) {
+            return $q.reject(Error('Cancelled by user', 1001))
+          }
+
+          // Convert raw canvas data to a base64 encoded png
+          var canvas = document.createElement('canvas')
+          var ctx = canvas.getContext('2d')
+
+          canvas.width = result.width
+          canvas.height = result.height
+          ctx.clearRect(0, 0, canvas.width, canvas.height)
+          ctx.putImageData(result, 0, 0)
+
+          this.currentSignature = canvas.toDataURL('image/png')
+          return this.currentSignature
+
+          // Let's resize this?
+          // var resizer = new Image()
+          //   , q = $q.defer()
+
+          // resizer.onload = function () {
+          //   var reCanvas = document.createElement('canvas')
+
+          //   reCanvas.width = canvas.width / 2
+          //   reCanvas.height = canvas.height / 2
+
+          //   reCanvas.getContext('2d').drawImage(resizer, 0, 0, reCanvas.width, reCanvas.height)
+
+          //   this.currentSignature = reCanvas.toDataURL('image/jpeg', 0.5)
+          //   q.resolve(this.currentSignature)
+          // }
+
+          // resizer.src = this.currentSignature
+
+        // return q.promise
+        }))
+    }
+
+    // Singleton Instance
+    return new Data()
+  })
+
+/* global angular, cordova */
+'use strict'
+angular.module('main')
+  .factory('Barcode', function ($log, $q) {
+    return {
+      scan: function () {
+        return $q(function (resolve, reject) {
+          cordova.plugins.barcodeScanner.scan(resolve, reject)
+        })
+      }
+    }
+
+  })
+
+/* global angular, moment */
+'use strict'
+angular.module('main')
+  .controller('View', function ($scope, $location, $log, $ionicModal, Data, Config) {
+    $scope.ENV = Config.ENV
+    $scope.BUILD = Config.BUILD
+    $scope.data = Data
+
+    $scope.showDelete = false
+    $scope.showReorder = false
+    $scope.listCanSwipe = true
+
+    $scope.signatures = []
+
+    $log.log('[View] Init', this)
+
+    console.log('[View] Config :: ' + JSON.stringify(Config))
+
+    $scope.toggleDelete = function () {
+      $scope.showDelete = !$scope.showDelete
+    }
+
+    $scope.delete = function (item) {
+      Data.delete(item.id)
+      $scope.refresh()
+    }
+
+    $scope.refresh = function (filter) {
+      $scope.signatures = Data.find(filter || {})
+        .map(function (o) {
+          // Pretty fields
+          o._title = o.name.join(', ')
+          o._created = moment(o.created)
+
+          // Pretty data list
+          var display = []
+
+          Object.keys(o).forEach(function (k) {
+            var label = k.replace(/_/g, ' ')
+
+            if (o.hasOwnProperty(k) && !/^(id|version|created|signature|vCard|_)/i.test(k)) {
+              if (angular.isArray(o[k])) {
+                display.push({ label: label, value: o[k].join(', ') })
+
+              } else if (angular.isObject(o[k])) {
+                display.push({
+                  label: label,
+                  value: Object.keys(o[k]).map(function (i) {
+                    return i.replace(/_/g, ' ') + ': ' + o[k][i]
+                  }).join(', ')
+                })
+
+              } else {
+                display.push({ label: label, value: o[k] })
+              }
+            }
+          })
+
+          o._display = display
+
+          // done
+          return o
+        })
+    }
+
+    // Modal
+
+    $ionicModal.fromTemplateUrl('detail-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.modal = modal
+    })
+
+    $scope.openModal = function (detail) {
+      if (detail) {
+        $scope.modal.scope.detail = detail
+        $scope.modal.show()
+      }
+    }
+
+    $scope.closeModal = function () {
+      $scope.modal.hide()
+      $scope.modal.scope.detail = null
+    }
+
+    $scope.$on('$destroy', function () {
+      $scope.modal.remove()
+      $scope.modal.scope.detail = null
+    })
+
+    // Init
+
+    $scope.refresh()
+  })
+
+/* global angular */
+'use strict'
+angular.module('main')
+  .controller('Start', function ($scope, $location, $log, Data, Config) {
+    // bind data from service
+    $scope.ENV = Config.ENV
+    $scope.BUILD = Config.BUILD
+
+    $log.log('[Start] Init', this)
+
+    // Data.getAll().then(function (result) {
+    //   $log.info('[Start] Current signature records', result)
+    // })
+
+    $scope.goto = function (path) {
+      console.log('[Start] Going to ' + path)
+      $location.path(('/' + path).replace(/\/+/g, '/'))
+    }
+  })
+
+/* global angular */
+'use strict'
+angular.module('main')
+  .controller('Scan', function ($scope, $window, $location, $log, Data, Config) {
+    $log.log('[Scan] Init', this)
+
+    $scope.ENV = Config.ENV
+    $scope.BUILD = Config.BUILD
+    $scope.data = Data
+    $scope.scanData = null
+    $scope.signData = null
+    $scope.scanError = null
+    $scope.signError = null
+
+    $scope.scanning = false
+    $scope.signing = false
+    $scope.saving = false
+    $scope.saved = false
+    $scope.saveError = null
+
+    // UI state switches
+
+    $scope.isActive = function () {
+      return $scope.scanning ||
+        $scope.signing ||
+        $scope.saving
+    }
+
+    $scope.showScan = function () {
+      return ($scope.scanData || $scope.scanning) && !$scope.scanError
+    }
+
+    $scope.showScanError = function () {
+      return !!$scope.scanError
+    }
+
+    $scope.showSignPrompt = function () {
+      return !$scope.isActive() &&
+        $scope.scanData &&
+        !$scope.scanError &&
+        !$scope.signData &&
+        !$scope.signError
+    }
+
+    $scope.showSignature = function () {
+      return ($scope.signData || $scope.signing) && !$scope.signError
+    }
+
+    $scope.showSignError = function () {
+      return !!$scope.signError
+    }
+
+    $scope.showSavePrompt = function () {
+      return !$scope.isActive() &&
+        !$scope.saved &&
+        Data.isValid()
+    }
+
+    $scope.showSaveSuccess = function () {
+      return $scope.saved
+    }
+
+    // Scan and Sign functionality
+    $scope.reset = function () {
+      Data.reset()
+
+      $scope.scanData = null
+      $scope.signData = null
+      $scope.scanError = null
+      $scope.signError = null
+
+      $scope.scanning = false
+      $scope.signing = false
+      $scope.saved = false
+      $scope.saveError = null
+    }
+
+    $scope.scan = function () {
+      $scope.reset()
+      $scope.scanning = true
+
+      return Data.scan()
+        .then(function (result) {
+          $log.log('[Scan] Scan result: ' + JSON.stringify(result))
+
+          $scope.scanning = false
+          $scope.scanData = []
+
+          // Prettify the data for showing
+          Object.keys(result).forEach(function (k) {
+            var label = k.replace(/_/g, ' ')
+
+            if (result.hasOwnProperty(k) && !/^(vCard|version)$/i.test(k)) {
+              if (angular.isArray(result[k])) {
+                $scope.scanData.push({ label: label, value: result[k].join(', ') })
+
+              } else if (angular.isObject(result[k])) {
+                $scope.scanData.push({
+                  label: label,
+                  value: Object.keys(result[k]).map(function (i) {
+                    return i.replace(/_/g, ' ') + ': ' + result[k][i]
+                  }).join(', ')
+                })
+
+              } else {
+                $scope.scanData.push({ label: label, value: result[k] })
+              }
+            }
+          })
+
+        })
+        .catch(function (err) {
+          $log.error('[Scan] Scan error: ' + JSON.stringify(err && err.message || err))
+
+          $scope.scanning = false
+
+          if (err.message === 'Cancelled by user') {
+            $location.path('/start')
+            return
+          }
+
+          $scope.scanError = err && err.message || err
+        })
+    }
+
+    $scope.sign = function () {
+      $scope.signing = true
+      $scope.signData = null
+      $scope.signError = null
+
+      return Data.sign()
+        .then(function (result) {
+          $log.log('[Scan] Sign result: ' + JSON.stringify(result))
+
+          $scope.signing = false
+          $scope.signData = result
+        })
+        .catch(function (err) {
+          $log.error('[Scan] Sign error: ' + JSON.stringify(err && err.message || err))
+
+          $scope.signing = false
+
+          if (err.message === 'Cancelled by user') {
+            // Do nothing, they should be able to sign again.
+            return
+          }
+
+          $scope.signError = err && err.message || err
+        })
+    }
+
+    $scope.save = function () {
+      $scope.saving = true
+      $scope.saveError = null
+
+      Data.save()
+        .then(function (result) {
+          $log.log('[Scan] Saved: ' + JSON.stringify(result))
+
+          $scope.saving = false
+          $scope.saved = true
+        })
+        .catch(function (err) {
+          $log.error('[Scan] Save error: ' + JSON.stringify(err && err.message || err))
+
+          $scope.saving = false
+          $scope.saveError = err && err.message || err
+        })
+    }
+
+    // Init
+    $scope.scan()
+  })
+
+/* global angular */
+'use strict'
+/*eslint-disable spaced-comment */
+angular.module('main')
+  .constant('Config', {
+    // gulp environment: injects environment vars
+    // https://github.com/mwaylabs/generator-m#gulp-environment
+    ENV: {
+      /*inject-env*/
+      'SERVER_URL': 'https://DEVSERVER/api'
+      /*endinject*/
+    },
+
+    // gulp build-vars: injects build vars
+    // https://github.com/mwaylabs/generator-m#gulp-build-vars
+    BUILD: {
+      /*inject-build*/
+      /*endinject*/
+    }
+  })
+/*eslint-enable spaced-comment */
+
+/* global angular */
+'use strict'
+
+// window.plugins.orientationLock.lock('landscape')
+
+angular.module('Re:Store', [
+  // your modules
+  'main'
+])
